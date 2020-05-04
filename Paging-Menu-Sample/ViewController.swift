a2f0e7a183e6317e4616d7443a6e8a5866dd52b6
Dai Kawahara <skbsporta7@gmail.com>
2020-05-05 03:04:50 +0900

    Initial commit

---
+++ Paging-Menu-Sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Paging-Menu-Sample
+//
+//  Created by kawaharadai on 2020/05/05.
+//  Copyright © 2020 kawaharadai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
